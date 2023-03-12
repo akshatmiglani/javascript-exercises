e746c27e509d3a1e2c17a32914c1bb9ccefa915b
AkshatM <akshatmiglani.am@gmail.com>
2023-03-12 18:43:36 +0530

    More questions completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let revstr="";
+    str.toLowerCase();
+    for(let i=str.length-1;i>=0;i--){
+        revstr+=str[i];
+    }
+    if(str === revstr){
+        return true
+    }
+    else{
+        return false
+    }
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n<0)return "OOPS"
+    if(n===0)return 0
+    let n1=0;
+    let n2=1;
+    for (let i = 1; i < n; i++) {
+        const temp = n2;
+        n2 = n1 + n2;
+        n1 = temp;
+      }
+      return n2;
 };
 
 // Do not edit below this line
